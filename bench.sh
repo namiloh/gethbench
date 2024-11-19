af3927aeecc93a337b88245de6e6d5d962ca4460
M H Swende <martin.alias@swende.se>
2024-11-19 14:36:50 +0100

    benches

--- bench.sh
+++ bench.sh
@@ -1,10 +1,11 @@
 
 NAME="bench-$(git rev-parse HEAD).log"
-
 cur=`pwd`
 cd ../go-ethereum
 go test ./... -run -  -bench . -benchmem -timeout 60m -skip \
   BenchmarkSearch128\|BenchmarkSearch512Layers\|BenchmarkSearch1Layer\|BenchmarkPersist\|BenchmarkTraceLogging\|BenchmarkWriteAncientBlocks\|BenchmarkTransactionTrace \
-  | tee $cur/$NAME
+  | tee $cur/benches/$NAME
+
+curl -d "bench done" ntfy.sh/gethbench
 
 cd $cur
