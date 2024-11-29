2edfcdca883eb23f48579d9b8441f48ae6dc40da
M H Swende <martin.alias@swende.se>
2024-11-29 08:56:13 +0100

    fix

--- bench.sh
+++ bench.sh
@@ -1,9 +1,12 @@
 
-NAME="bench-$(git rev-parse HEAD).log"
 cur=`pwd`
 cd ../go-ethereum
+NAME="bench-$(git rev-parse HEAD).log"
+
+curl -d "starting bench $NAME" ntfy.sh/gethbench
+
 go test ./... -run -  -bench . -benchmem -timeout 60m -skip \
-  BenchmarkSearch128\|BenchmarkSearch512Layers\|BenchmarkSearch1Layer\|BenchmarkPersist\|BenchmarkTraceLogging\|BenchmarkWriteAncientBlocks\|BenchmarkTransactionTrace \
+  BenchmarkSearch128\|BenchmarkSearch512Layers\|BenchmarkSearch1Layer\|BenchmarkPersist\|BenchmarkTraceLogging\|BenchmarkWriteAncientBlocks\|BenchmarkTransactionTrace\|BenchmarkTracerStepVsCallFrame \
   | tee $cur/benches/$NAME
 
 curl -d "bench done" ntfy.sh/gethbench
